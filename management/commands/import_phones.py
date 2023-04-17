e5d808d54eb3fd8b40d310a1ee19abe192c198ff
Arckadich <kozlowva@gmail.com>
2023-04-17 18:02:18 +0300

    First commit

--- management/commands/import_phones.py
+++ management/commands/import_phones.py
@@ -1,17 +1,27 @@
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
-
 class Command(BaseCommand):
+    help = 'Imports phones from a CSV file'
+
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('file', type=str, help='The CSV file to import')
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        file_path = options['file']
+
+        with open(file_path, 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            phone_obj = Phone(
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists']
+            )
+            phone_obj.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully imported phones from CSV file'))
